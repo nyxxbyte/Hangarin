10e7c61ebf36e5f0bbd4c406832a3b86f5c51b5b
nyxxbyte <nyxelmido@gmail.com>
2026-09-20 12:58:38 +0800

    Initial Webpage Design

--- tracker/views.py
+++ tracker/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Task
 
-# Create your views here.
+def home(request):
+
+    tasks = Task.objects.all()
+
+    return render(request, 'home.html', {'tasks': tasks})
